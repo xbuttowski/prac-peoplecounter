ce2016d7ba03ea416c2b37476364044abbcdfde7
xbuttowski <cloudxstrife9@gmail.com>
2024-03-06 23:54:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-// intialize the count as 0
-// listen for clicks on the increment button
-// increment the count variable when the button is clicked
-// change the count-el in the HTML to reflect the new count
-let count = 0;
-let countEl = document.getElementById("count-el");
-let saveEl = document.getElementById("save-el");
+let count = 0; //intialize the count as 0
+let countEl = document.getElementById("count-el"); //for the count
+let saveEl = document.getElementById("save-el"); //for the save
+let totalInput = []; //empty array
+let totalEl = document.getElementById("total-el"); //for the total
 
 function increment(){
     count += 1;
@@ -17,14 +15,24 @@ function remove(){
 }
 
 function save(){
-    let countStr = count + " - ";
-    saveEl.textContent += countStr;
+    let countStr = count;
+    let totalNum = 0;
+    saveEl.textContent += countStr + " ~ ";
+    totalInput.push(countStr); //push the count into the array
+    for(let i = 0; i < totalInput.length; i++){
+        totalNum += totalInput[i]; //for loop to add up the total
+    }
+    totalEl.textContent = `People entered in total: ${totalNum}`;
     count = 0;
     countEl.textContent = count;
 }
 
 function reset(){
-    count = 0;
+    count = 0; //reset the count to zero
     countEl.textContent = count;
     saveEl.textContent = "Previous entries: ";
+    totalNum = 0; //reset the totalNum to zero
+    totalInput = []; //empty the array
+    totalEl.textContent = `People entered in total: ${totalNum}`;
 }
+
